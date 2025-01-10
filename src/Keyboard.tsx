9887b7509d4cfe7824c2363ee346c855d380705b
ololade <belloololade2003@gmail.com>
2025-01-10 01:28:57 +0100

    completed

---
+++ src/Keyboard.tsx
@@ -0,0 +1,66 @@
+import styles from "./Keyboard.module.css";
+
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+type KeyboardProps = {
+  activeLetters: string[];
+  inactiveLetters: string[];
+  addGuessedLetters: (letter:string) => void
+  disabled?: boolean
+};
+
+const Keyboard = ({ activeLetters, inactiveLetters, addGuessedLetters, disabled }: KeyboardProps) => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map((key) => {
+        const isActive = activeLetters.includes(key)
+        const isInactive = inactiveLetters.includes(key)
+        return (
+          <button
+            key={key}
+            onClick={() => addGuessedLetters(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ''}  ${isInactive ? styles.inactive : ''}`}
+            disabled={isActive || isInactive || disabled}
+          >
+            {key}
+          </button>
+        );
+})}
+    </div>
+  );
+}; 
+
+export default Keyboard;
